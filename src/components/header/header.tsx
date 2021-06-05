591e9e6d244e1df4213ebb14aada8e1e6c5cb372
Wojciech Cendrzak <23259795+WojciechCendrzak@users.noreply.github.com>
2021-06-05 23:56:42 +0200

    layout updated

--- src/components/header/header.tsx
+++
@@ -1,3 +0,0 @@
-import './header.styles.css';
-
-export const Header = () => <div className="header">Book search</div>;
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,12 +1,23 @@
-import { Header } from '../header/header';
-
-import './layout.styles.css';
+import styled from 'styled-components';
+import { APP_MARGIN } from '../../styles/layout';
 
 export const Layout: React.FC = ({ children }) => (
-  <>
-    <Header />
-    <div className="main">
-      <div className="content">{children}</div>
-    </div>
-  </>
+  <Main>
+    <Content>{children}</Content>
+  </Main>
 );
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: ${APP_MARGIN};
+`;
+
+const Content = styled.div`
+  width: 600px;
+
+  @media only screen and (max-width: 768px) {
+    width: 90%;
+  }
+`;
