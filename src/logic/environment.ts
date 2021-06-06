56ec55520ea70652152ae0b8276034b83bdb7f06
Wojciech Cendrzak <23259795+WojciechCendrzak@users.noreply.github.com>
2021-06-06 02:42:56 +0200

    environent updated

--- src/logic/environment.ts
+++ src/logic/environment.ts
@@ -2,4 +2,5 @@ export const isProduction = () => process.env.NODE_ENV === 'production';
 
 export const getEnv = () => ({
   NODE_ENV: process.env.NODE_ENV,
+  REACT_APP_IT_BOOK_STORE_API: process.env.REACT_APP_IT_BOOK_STORE_API,
 });
--- src/logic/store/book/book.api.ts
+++ src/logic/store/book/book.api.ts
@@ -1,9 +1,12 @@
+import { getEnv } from '../../environment';
 import { BookDetails, BookSearchResult } from './book.models';
+
+const bookUrl = getEnv().REACT_APP_IT_BOOK_STORE_API || '';
 
 export const bookApi = {
   fetchBooks: async (searchPhrase: string): Promise<BookSearchResult> =>
-    (await fetch(`/search/${searchPhrase}`)).json(),
+    (await fetch(`${bookUrl}/search/${searchPhrase}`)).json(),
 
   fetchBook: async (isbn13: string): Promise<BookDetails> =>
-    (await fetch(`/books/${isbn13}`)).json(),
+    (await fetch(`${bookUrl}/books/${isbn13}`)).json(),
 };
