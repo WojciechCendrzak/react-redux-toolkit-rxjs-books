4b5e4b7349a3a6da49d87f66e8c2c1669bb3999d
Wojciech Cendrzak <23259795+WojciechCendrzak@users.noreply.github.com>
2021-06-10 15:20:22 +0200

    store simplyfied

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,47 +1,16 @@
-import { Action, combineReducers } from '@reduxjs/toolkit';
-import {
-  applyMiddleware,
-  compose,
-  createStore,
-  Middleware,
-  Store,
-  StoreEnhancer,
-} from 'redux';
+import { Action } from '@reduxjs/toolkit';
 import * as reduxLogger from 'redux-logger';
 import { createEpicMiddleware } from 'redux-observable';
-import { isProduction } from '../logic/environment';
-import { WindowWithDevTools } from '../window-with-dev-tools';
 import { reducers, StoreState } from './app.reducers';
 import { rootEpic } from './app.epics';
+import { configureStore } from '@reduxjs/toolkit';
 
 const epicMiddleware = createEpicMiddleware<Action, Action, StoreState>();
+const loggerMiddleware = reduxLogger.createLogger({ collapsed: true });
 
-const middlewares: Middleware[] = [epicMiddleware];
-let enhancer: StoreEnhancer;
+export const store = configureStore({
+  reducer: reducers,
+  middleware: [epicMiddleware, loggerMiddleware],
+});
 
-if (isProduction()) {
-  enhancer = compose(applyMiddleware(...middlewares));
-} else {
-  const logger: Middleware = reduxLogger.createLogger({ collapsed: true });
-  middlewares.push(logger);
-  const reduxDevToolsExtensionCompose = (
-    window as unknown as WindowWithDevTools
-  ).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-  const composeEnhancers = reduxDevToolsExtensionCompose
-    ? reduxDevToolsExtensionCompose({})
-    : compose;
-  enhancer = composeEnhancers(applyMiddleware(...middlewares));
-}
-
-const reducer = combineReducers(reducers);
-
-export const configureStore = () => {
-  const store: Store = createStore(reducer, enhancer);
-  epicMiddleware.run(rootEpic);
-
-  return {
-    store,
-  };
-};
-
-export const { store } = configureStore();
+epicMiddleware.run(rootEpic);
--- src/window-with-dev-tools.ts
+++
@@ -1,7 +0,0 @@
-import { StoreEnhancer } from 'redux';
-
-export interface WindowWithDevTools extends Window {
-  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: (
-    ...middlewares: object[]
-  ) => (options: any) => StoreEnhancer;
-}
